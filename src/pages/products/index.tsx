ede58e650e44bfe8f652a7ed6d942471efbfc197
ostapvasilenko <>
2023-11-22 18:11:13 +0200

    Update index.tsx

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -8,36 +8,34 @@ import Card from "@/components/productCard";
 // modules
 import { Products as Prod } from "@/modules/products";
 
-const getAllProducts = async () => {
-  return await fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((json) => {
-      return json;
-    });
-};
+interface Props {
+  prods: Product[];
+}
 
-const Products = () => {
-  // init
-  const prod = new Prod();
+const Products = ({ prods }: Props) => {
   // states
-  const [products, setProducts] = useState<Product[] | null>(null);
-  // load
-  useEffect(() => {
-    // getAllProducts().then((data: Product[]) => setProducts(data));
-    prod.getData("posts").then((data: Product[]) => setProducts(data));
-  }, []);
+  const [products, setProducts] = useState<Product[] | null>(prods);
   return (
     <>
       <div className={s.products}>Products</div>
-      {products ? (
-        products.map((product: Product) => {
-          return <Card key={product.id} data={product} />;
-        })
-      ) : (
-        <div>Products Loading....</div>
-      )}
+      <div>
+        {products ? (
+          products.map((product: Product) => {
+            return <Card key={product.id} data={product} />;
+          })
+        ) : (
+          <div>Products Loading....</div>
+        )}
+      </div>
     </>
   );
 };
+
+// SSR
+export async function getServerSideProps() {
+  const prod = new Prod();
+  const data = await prod.getData("posts");
+  return { props: { prods: data } };
+}
 
 export default Products;
